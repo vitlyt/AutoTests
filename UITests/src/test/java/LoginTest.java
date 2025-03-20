8b591039eaaa0e8ce11cb2aa4aedd12fd03f57e4
vitlyt <cshinepower@gmail.com>
2025-03-20 16:17:50 +0200

    added dataprovider for auth in UI

--- UITests/src/test/java/LoginTest.java
+++ UITests/src/test/java/LoginTest.java
@@ -1,8 +1,11 @@
+import dataproviders.AuthDataProvider;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageobjects.BasePage;
 import pageobjects.LoginPage;
 import pageobjects.RecommendationsPage;
+
+import java.util.Map;
 
 import static org.testng.Assert.*;
 
@@ -15,17 +18,17 @@ public class LoginTest extends BaseTest {
         loginPage = new LoginPage();
     }
 
-    @Test(priority = 0)
-    public void testLoginFailed(){
-        loginPage.login("nonexixtent@mail.com", "test");
+    @Test(priority = 0, dataProvider = "invalid-auth-data-provider", dataProviderClass = AuthDataProvider.class)
+    public void testLoginFailed(Map<String, String> params){
+        loginPage.login(params.get(AuthDataProvider.USERNAME_FIELD), AuthDataProvider.PASSWORD_FIELD);
         assertEquals(loginPage.getErrorText(), "Ви ввели невірний пароль або логін");
     }
 
 
-    @Test(priority = 1)
-    public void testLoginSucceeded(){
+    @Test(priority = 1, dataProvider = "valid-auth-data-provider", dataProviderClass = AuthDataProvider.class)
+    public void testLoginSucceeded(Map<String, String> params){
         loginPage.reload();
-        BasePage page = loginPage.login(System.getProperty("username"), System.getProperty("password"));
+        BasePage page = loginPage.login(params.get(AuthDataProvider.USERNAME_FIELD), AuthDataProvider.PASSWORD_FIELD);
         assertTrue(page instanceof RecommendationsPage && page.isLoaded());
     }
 
---
+++ UITests/src/test/java/dataproviders/AuthDataProvider.java
@@ -0,0 +1,29 @@
+package dataproviders;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthDataProvider {
+    public static final String USERNAME_FIELD = "username";
+    public static final String PASSWORD_FIELD = "password";
+
+    @DataProvider(name = "invalid-auth-data-provider")
+    public Object[][] getValidCredentials(){
+        Map<String, String> params = new HashMap<>(){{
+            put(USERNAME_FIELD, "nonexistent@test.com");
+            put(PASSWORD_FIELD, "password123");
+        }};
+        return new Object[][] {{params}};
+    }
+    @DataProvider(name = "valid-auth-data-provider")
+    public Object[][] getInValidCredentials(){
+        Map<String, String> params = new HashMap<>(){{
+            put(USERNAME_FIELD, System.getProperty("username"));
+            put(PASSWORD_FIELD, System.getProperty("password"));
+        }};
+
+        return new Object[][] {{params}};
+    }
+}
